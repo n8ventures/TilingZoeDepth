87b59fa1b292f9a1bfd2f2b0de6b3a43090c0e6e
n8ventures <nate@n8ventures.dev>
2024-08-07 02:07:42 +0800

    forgot to uncomment xd

--- osx-build.py
+++ osx-build.py
@@ -7,25 +7,25 @@ import shutil
 
 app_name = 'Tiled ZoeDepth GUI' 
 
-# # Step 1: Remove old build/dist
-# subprocess.run(['rm', '-rf', 'build', 'dist'])
-# # Step 2: build .app
-# subprocess.run(['python', 'setup.py', 'py2app'])
+# Step 1: Remove old build/dist
+subprocess.run(['rm', '-rf', 'build', 'dist'])
+# Step 2: build .app
+subprocess.run(['python', 'setup.py', 'py2app'])
 
-# # Step 3: Copy torch folder from site-packages and replace torch version inside .app
-# site_packages = site.getsitepackages()[0]
-# python_version = f"{site_packages.split('/')[-2]}"
-# app_path = f"dist/{app_name}.app/Contents/Resources/lib/{python_version}"
+# Step 3: Copy torch folder from site-packages and replace torch version inside .app
+site_packages = site.getsitepackages()[0]
+python_version = f"{site_packages.split('/')[-2]}"
+app_path = f"dist/{app_name}.app/Contents/Resources/lib/{python_version}"
 
-# torch_site_path = os.path.join(site_packages, 'torch')
-# torch_app_path = os.path.join(app_path, 'torch')
+torch_site_path = os.path.join(site_packages, 'torch')
+torch_app_path = os.path.join(app_path, 'torch')
 
-# if os.path.exists(torch_app_path):
-#     shutil.rmtree(torch_app_path)
+if os.path.exists(torch_app_path):
+    shutil.rmtree(torch_app_path)
 
-# shutil.copytree(torch_site_path, torch_app_path)
+shutil.copytree(torch_site_path, torch_app_path)
 
-#Step 5: Create DMG
+# Step 5: Create DMG
 if os.path.exists(f'./{app_name}.dmg'):
     os.remove(f'{app_name}.dmg')
 
